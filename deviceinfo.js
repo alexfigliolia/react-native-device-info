9304c175c83d2daca74b3c68e6d123134e2fd9c8
alexfigliolia <alexfigliolia@gmail.com>
2019-03-16 13:06:35 -0400

    Notch device search is O(1), cache brand and model

--- deviceinfo.js
+++ deviceinfo.js
@@ -3,190 +3,60 @@
  */
 import { Platform, NativeModules, Dimensions } from 'react-native';
 
-var RNDeviceInfo = NativeModules.RNDeviceInfo;
+let RNDeviceInfo = NativeModules.RNDeviceInfo;
 
 if (!RNDeviceInfo && Platform.OS === 'web') {
   RNDeviceInfo = require('./web');
 }
 
-const devicesWithNotch = [
-  {
-    brand: 'Apple',
-    model: 'iPhone X',
-  },
-  {
-    brand: 'Apple',
-    model: 'iPhone XS',
-  },
-  {
-    brand: 'Apple',
-    model: 'iPhone XS Max',
-  },
-  {
-    brand: 'Apple',
-    model: 'iPhone XR',
-  },
-  {
-    brand: 'Asus',
-    model: 'ZenFone 5',
-  },
-  {
-    brand: 'Asus',
-    model: 'ZenFone 5z',
-  },
-  {
-    brand: 'google',
-    model: 'Pixel 3 XL',
-  },
-  {
-    brand: 'Huawei',
-    model: 'P20',
-  },
-  {
-    brand: 'Huawei',
-    model: 'P20 Plus',
-  },
-  {
-    brand: 'Huawei',
-    model: 'P20 Lite',
-  },
-  {
-    brand: 'Huawei',
-    model: 'ANE-LX1',
-  },
-  {
-    brand: 'Huawei',
-    model: 'Honor 10',
-  },
-  {
-    brand: 'Huawei',
-    model: 'Nova 3',
-  },
-  {
-    brand: 'Huawei',
-    model: 'Nova 3i',
-  },
-  {
-    brand: 'Leagoo',
-    model: 'S9',
-  },
-  {
-    brand: 'LG',
-    model: 'G7',
-  },
-  {
-    brand: 'LG',
-    model: 'G7 ThinQ',
-  },
-  {
-    brand: 'LG',
-    model: 'G7+ ThinQ',
-  },
-  {
-    brand: 'LG',
-    model: 'LM-Q910', //G7 One
-  },
-  {
-    brand: 'LG',
-    model: 'LM-G710', //G7 ThinQ
-  },
-  {
-    brand: 'LG',
-    model: 'LM-V405', //V40 ThinQ
-  },
-  {
-    brand: 'Nokia',
-    model: '6.1 Plus',
-  },
-  {
-    brand: 'OnePlus',
-    model: '6',
-  },
-  {
-    brand: 'OnePlus',
-    model: 'A6003',
-  },
-  {
-    brand: 'ONEPLUS',
-    model: 'A6000',
-  },
-  {
-    brand: 'OnePlus',
-    model: 'OnePlus A6003',
-  },
-  {
-    brand: 'OnePlus',
-    model: 'ONEPLUS A6013',
-  },
-  {
-    brand: 'OnePlus',
-    model: 'ONEPLUS A6000',
-  },
-  {
-    brand: 'Oppo',
-    model: 'R15',
-  },
-  {
-    brand: 'Oppo',
-    model: 'R15 Pro',
-  },
-  {
-    brand: 'Oppo',
-    model: 'F7',
-  },
-  {
-    brand: 'Oukitel',
-    model: 'U18',
-  },
-  {
-    brand: 'Sharp',
-    model: 'Aquos S3',
-  },
-  {
-    brand: 'Vivo',
-    model: 'V9',
-  },
-  {
-    brand: 'Vivo',
-    model: 'X21',
-  },
-  {
-    brand: 'Vivo',
-    model: 'X21 UD',
-  },
-  {
-    brand: 'xiaomi',
-    model: 'MI 8',
-  },
-  {
-    brand: 'xiaomi',
-    model: 'MI 8 Explorer Edition',
-  },
-  {
-    brand: 'xiaomi',
-    model: 'MI 8 SE',
-  },
-  {
-    brand: 'xiaomi',
-    model: 'MI 8 UD',
-  },
-  {
-    brand: 'xiaomi',
-    model: 'MI8Lite',
-  },
-  {
-    brand: 'xiaomi',
-    model: 'POCO F1',
-  },
-  {
-    brand: 'xiaomi',
-    model: 'POCOPHONE F1',
-  },
-  {
-    brand: 'xiaomi',
-    model: 'Redmi 6 Pro',
-  },
-];
+const devicesWithNotch = {
+  'apple iphone x': true,
+  'apple iphone xs': true,
+  'apple iphone xs max': true,
+  'apple iphone xr': true,
+  'asus zenfone 5': true,
+  'asus zenfone 5z': true,
+  'google pixel 3 xl': true,
+  'Huawei p20': true,
+  'Huawei p20 plus': true,
+  'Huawei p20 lite': true,
+  'Huawei ane-lx1': true,
+  'Huawei honor 10': true,
+  'Huawei nova 3': true,
+  'Huawei nova 3i': true,
+  'leagoo s9': true,
+  'lg g7': true,
+  'lg g7 thinq': true,
+  'lg g7+ thinq': true,
+  'lg lm-q910': true,
+  'lg lm-q710': true,
+  'lg lm-q405': true,
+  'nokia 6.1 plus': true,
+  'oneplus 6': true,
+  'oneplus a6003': true,
+  'oneplus a6000': true,
+  'oneplus oneplus a6003': true,
+  'oneplus oneplus a6013': true,
+  'oneplus oneplus a6000': true,
+  'oppo r15': true,
+  'oppo r15 pro': true,
+  'oppo f7': true,
+  'oukitel u18': true,
+  'sharp aquos s3': true,
+  'vivo v9': true,
+  'vivo x21': true,
+  'vivo x21 ud': true,
+  'xiaomi mi 8': true,
+  'xiaomi mi 8 explorer edition': true,
+  'xiaomi mi 8 se': true,
+  'xiaomi mi 8 ud': true,
+  'xiaomi mi8lite': true,
+  'xiaomi poco f1': true,
+  'xiaomi pocophone f1': true,
+  'xiaomi redmi 6 pro': true,
+};
+
+const { brand, model } = RNDeviceInfo;
 
 export default {
   getUniqueID: function() {
@@ -271,7 +141,7 @@ export default {
     return RNDeviceInfo.isPinOrFingerprintSet;
   },
   hasNotch: function() {
-    return devicesWithNotch.findIndex(item => item.brand.toLowerCase() === RNDeviceInfo.brand.toLowerCase() && item.model.toLowerCase() === RNDeviceInfo.model.toLowerCase()) !== -1;
+    return brand.toLowerCase() + model.toLowerCase() + '' in devicesWithNotch;
   },
   getFirstInstallTime: function() {
     return RNDeviceInfo.firstInstallTime;
